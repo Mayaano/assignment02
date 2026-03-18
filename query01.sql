5991f15a075c86b6741ecfa4414d8dfe88ebb5ab
zhe fang <130455256+Mayaano@users.noreply.github.com>
2026-03-18 14:57:08 -0400

    fix: qualify column references and use CTEs for lint compliance

--- query01.sql
+++ query01.sql
@@ -1,12 +1,22 @@
+with stop_locations as (
+    select
+        stop_id::text as stop_id,
+        stop_name,
+        st_setsrid(
+            st_makepoint(stop_lon, stop_lat), 4326
+        )::geography as stop_geog
+    from septa.bus_stops
+)
+
 select
-    stops.stop_id::text as stop_id,
+    stops.stop_id,
     stops.stop_name,
     sum(pop.total)::integer as estimated_pop_800m
-from septa.bus_stops as stops
+from stop_locations as stops
 inner join census.blockgroups_2020 as bg
     on st_dwithin(
         bg.geog,
-        st_setsrid(st_makepoint(stops.stop_lon, stops.stop_lat), 4326)::geography,
+        stops.stop_geog,
         800
     )
 inner join census.population_2020 as pop
